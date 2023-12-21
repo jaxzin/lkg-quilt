e21c5c9449a5a0d92e15138fd09b7fd93758be83
Brian R. Jackson <brian@jaxzin.com>
2023-12-21 17:26:05 -0500

    Fixed focal processing. Count frames rather than depending on metadata.

--- lkg_quilt/__main__.py
+++ lkg_quilt/__main__.py
@@ -2,14 +2,41 @@ import os
 import re
 import argparse
 import ffmpeg
+import math
 
+def count_frames(video_path, width, height, verbose):
+    frame_count = 0
+    try:
+        # Open the video file and start processing frames
+        process = (
+            ffmpeg
+            .input(video_path)
+            .output('pipe:', format='rawvideo', pix_fmt='rgb24', loglevel='verbose' if verbose else 'error')
+            .run_async(pipe_stdout=True)
+        )
 
-def get_video_info(filename):
+        while True:
+            in_bytes = process.stdout.read(3 * width * height)
+            if not in_bytes:
+                break
+            frame_count += 1
+
+        process.wait()
+
+    except ffmpeg.Error as e:
+        print("FFmpeg error:", e)
+
+    return frame_count
+
+def get_video_info(filename, verbose):
     probe = ffmpeg.probe(filename)
     video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
     width = int(video_stream['width'])
     height = int(video_stream['height'])
-    frame_count = int(video_stream['nb_frames']) if 'nb_frames' in video_stream else 1
+    nb_frames = int(video_stream['nb_frames']) if 'nb_frames' in video_stream else 1
+    print(f'Counting frames because the metadata lies sometimes (nb_frames: {nb_frames})...')
+    frame_count = count_frames(filename, width, height, verbose)
+    print(f'Done counting, found {frame_count} frames')
 
     # Extract frame rate
     if 'r_frame_rate' in video_stream and video_stream['r_frame_rate'] != '0/0':
@@ -32,6 +59,49 @@ def get_video_info(filename):
 
     return width, height, frame_count, frame_rate, rotation
 
+def get_cropped_dimensions(orig_width, orig_height, target_aspect_ratio):
+    """
+    Calculate the dimensions of the largest rectangle (new_width, new_height) that fits within the
+    original dimensions (orig_width, orig_height) and has the specified aspect ratio (target_aspect_ratio).
+
+    Parameters:
+    orig_width (int): The original width of the video/image.
+    orig_height (int): The original height of the video/image.
+    target_aspect_ratio (float): The target aspect ratio (width/height).
+
+    Returns:
+    (int, int): The new width and height after cropping.
+    """
+
+    # Calculate the aspect ratio of the original dimensions
+    orig_aspect_ratio = orig_width / orig_height
+
+    if orig_aspect_ratio > target_aspect_ratio:
+        # If the original aspect ratio is wider than the target, adjust the width
+        new_width = int(orig_height * target_aspect_ratio)
+        new_height = orig_height
+    else:
+        # If the original aspect ratio is narrower than the target, adjust the height
+        new_width = orig_width
+        new_height = int(orig_width / target_aspect_ratio)
+
+    return new_width, new_height
+
+def extract_views_from_video(filter_stream, total_frames, total_views):
+    # Find the widest separation between frames we can use without dropping frames
+    # notes: ffmpeg frame numbers is zero-indexed, hense total_frames - 1
+    #        if we want X total_views, then there are only total_views-1 gaps between them
+    interval = max(1,
+                   math.floor((total_frames - 1) / (total_views - 1)))
+    start_frame = (int(((total_frames - 1) - (total_views - 1) * interval) / 2))
+    end_frame = start_frame + (total_views - 1) * interval
+    filter_stream = (
+        filter_stream.video.filter(
+            'select',
+            # Starting with the start_frame, select frames at interval until end_frame
+            f'gte(n,{start_frame})*lte(n,{end_frame})*not(mod(n-{start_frame},{interval}))')
+    )
+    return filter_stream
 
 def create_quilt_filter(
         row_count,
@@ -42,25 +112,40 @@ def create_quilt_filter(
         focus,
         rail,
         quilt_w,
-        quilt_h):
+        quilt_h,
+        verbose):
 
     # Adjusting if only one file is listed (changes ffmpeg behavior)
     if isinstance(input_pattern, list) and len(input_pattern) == 1:
         input_pattern = input_pattern[0]
 
     total_views = row_count * column_count
-    orig_width, orig_height, total_frames, frame_rate, rotation = get_video_info(input_pattern)
+    orig_width, orig_height, total_frames, frame_rate, rotation = get_video_info(input_pattern, verbose)
 
-    # When panning for focus, how much padding do we need?
-    padding_factor = ((focus + 1) * 2) + 1
+    crop_width, crop_height = get_cropped_dimensions(orig_width, orig_height, aspect_ratio)
+
     # When focusing, what the max number of pixels we need to shift the images by
-    max_focus_shift = orig_width * abs(focus * 2)
+    max_focus_shift = crop_width * abs(focus * 2)
+    # The width of pixels we need to do the focus shift
+    # (the cropped image width plus the max distance in pixels we need to shift to adjust the focus)
+    zoompan_width = orig_width + max_focus_shift
+
+    # The amount of padding we need around the original image to complete a full focus shift
+    # This is a scaling factor because the padding needs to be added to the top/bottom too since
+    # the ffmpeg zoompan filter can only zoom to the same aspect ratio as the input, so we pad all four sides
+    # of the original image and then zoom in so the original image fills the view, but has room off the left/right
+    # side for shifting outside of the original leftmost and rightmost views
+    padding_factor = (zoompan_width / orig_width)
+    pad_width = orig_width * padding_factor
+    focus_start_x = (pad_width - zoompan_width) / 2
+    shift_per_frame = max_focus_shift / total_views
+    #print(f'max_focus_shift:{max_focus_shift},orig_width:{orig_width},crop_width:{crop_width},zoompan_width:{zoompan_width}, pad_width:{int(orig_width * padding_factor)}, padding_factor:{padding_factor}, focus_start_x:{focus_start_x}, shift_per_frame:{shift_per_frame}')
 
     # Get the input views (this can be an imageset, a video set, or a single video (if rail is true)
     filter_stream = ffmpeg.input(input_pattern)
     # If the input is a photo rail video, we need to select the views from it as single frames
     if rail:
-        filter_stream = filter_stream.filter('select', f'not(mod(n,{int(total_frames / total_views)}))')
+        filter_stream = extract_views_from_video(filter_stream, total_frames, total_views)
 
     # A quick "ffmpeg-way" of reversing the order of the views...
     # ...is to flip the tiles and then reverse the flip after the tiling filter.
@@ -87,15 +172,15 @@ def create_quilt_filter(
         .filter('zoompan',
                 z=padding_factor,
                 x=f'if(eq(time,0),'
-                  f'{orig_width} * {(padding_factor - 1) / 2} - {max_focus_shift / 2},'
-                  f'px + {max_focus_shift / total_views})',
+                  f'{focus_start_x},'
+                  f'px + {shift_per_frame})',
                 y='ih/2-(ih/zoom/2)',
                 d=1,
-                s=f'{int(orig_width * padding_factor)}x{int(orig_height * padding_factor)}')
+                s=f'{orig_width}x{orig_height}')
         # Crop each tile down to the final aspect ratio
         .filter('crop',
-                w='iw',
-                h=f'iw / {aspect_ratio}')
+                w=crop_width,
+                h=crop_height)
         # Scale each tile down to the final size
         .filter('scale',
                 width=tile_w,
@@ -172,7 +257,8 @@ def main():
             args.focus,
             args.rail,
             args.width,
-            args.height
+            args.height,
+            args.verbose
         )
     except ffmpeg.Error:
         # ANSI escape codes
